34130ef6b8967874a9b310fabec8e34576adaa20
Steliyan003 <steliyan2rumenov@gmail.com>
2025-12-02 18:14:42 +0000

    Добавяне на персонализирана 404 страница

---
+++ laravel-todos/resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="py-16 text-center">
+        <h1 class="text-5xl font-bold mb-4">404</h1>
+        <p class="text-gray-600 text-lg">Страницата не е намерена.</p>
+    </div>
+</x-app-layout>
